5653e7cef5b382533a6b8f3d1e6c19b83cc116b0
Revathiv47 <83072263+Revathiv47@users.noreply.github.com>
2022-01-18 15:39:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const mongoose = require('mongoose')
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const PORT = process.env.PORT | 1337
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/full-mern-stack-video')
 
-app.post('/api/register', async (req, res) => {
+//const url = "mongodb+srv://admin:<Riya4798>@jwt.osp3y.mongodb.net/jwt?retryWrites=true&w=majority";
+
+mongoose.connect('mongodb://localhost:27017/full-mern-stack')
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log("DB connected");
+})
+
+app.post('/register', async (req, res) => {
 	console.log(req.body)
 	try {
 		const newPassword = await bcrypt.hash(req.body.password, 10)
@@ -19,6 +28,7 @@ app.post('/api/register', async (req, res) => {
 			name: req.body.name,
 			email: req.body.email,
 			password: newPassword,
+	
 		})
 		res.json({ status: 'ok' })
 	} catch (err) {
@@ -26,7 +36,7 @@ app.post('/api/register', async (req, res) => {
 	}
 })
 
-app.post('/api/login', async (req, res) => {
+app.post('/login', async (req, res) => {
 	const user = await User.findOne({
 		email: req.body.email,
 	})
@@ -88,6 +98,6 @@ app.post('/api/quote', async (req, res) => {
 	}
 })
 
-app.listen(1337, () => {
-	console.log('Server started on 1337')
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`)
 })
